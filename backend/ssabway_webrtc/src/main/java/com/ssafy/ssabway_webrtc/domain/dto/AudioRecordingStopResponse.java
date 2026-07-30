e7e2bae783acd1b2880f85879709fde7ff115070
강석진 <redmoon8888@gmail.com>
2026-07-30 10:30:34 +0900

    ✨feat: OpenVidu 녹화 상태 변경 웹훅 구현

--- backend/ssabway_webrtc/src/main/java/com/ssafy/ssabway_webrtc/domain/dto/AudioRecordingStopResponse.java
+++ backend/ssabway_webrtc/src/main/java/com/ssafy/ssabway_webrtc/domain/dto/AudioRecordingStopResponse.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class AudioRecordingStopResponse {
-    private String recordingid;
+    private String recordingId;
     private String sessionId;
     private String status;
 }
