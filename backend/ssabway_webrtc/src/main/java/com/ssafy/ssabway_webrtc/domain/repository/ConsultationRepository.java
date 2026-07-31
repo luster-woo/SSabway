08c2dd0d6b82de72c816a724ca0813f5f872435e
강석진 <redmoon8888@gmail.com>
2026-07-31 17:34:29 +0900

    🐛fix: 상담 취소 쿼리 수정

--- backend/ssabway_webrtc/src/main/java/com/ssafy/ssabway_webrtc/domain/repository/ConsultationRepository.java
+++ backend/ssabway_webrtc/src/main/java/com/ssafy/ssabway_webrtc/domain/repository/ConsultationRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.security.core.parameters.P;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface ConsultationRepository extends JpaRepository<Consultation, Long> {
@@ -75,7 +74,7 @@ public interface ConsultationRepository extends JpaRepository<Consultation, Long
         flushAutomatically = true
     )
     @Query("""
-        UPDATE consultation c
+        UPDATE Consultation c
         SET c.status = :nextStatus
         WHERE c.id = :consultationId
             AND c.status = :currentStatus
