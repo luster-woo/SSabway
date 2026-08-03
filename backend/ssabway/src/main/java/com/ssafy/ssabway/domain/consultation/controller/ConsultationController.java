539ee6778393ef62eeb5dca11bbaa6f1116b3891
황문규 <moongye2001a@gmail.com>
2026-08-03 14:20:37 +0900

    feat: 원본 상담 내역 조회 API 구현

--- backend/ssabway/src/main/java/com/ssafy/ssabway/domain/consultation/controller/ConsultationController.java
+++ backend/ssabway/src/main/java/com/ssafy/ssabway/domain/consultation/controller/ConsultationController.java
@@ -1,5 +1,6 @@
 package com.ssafy.ssabway.domain.consultation.controller;
 
+import com.ssafy.ssabway.domain.consultation.dto.response.ConsultationDetailResponse;
 import com.ssafy.ssabway.domain.consultation.dto.response.WaitingResponse;
 import com.ssafy.ssabway.domain.consultation.service.ConsultationService;
 import com.ssafy.ssabway.global.common.ApiResponse;
@@ -27,5 +28,14 @@ public class ConsultationController {
 
         return ResponseEntity.ok(
                 ApiResponse.ok("대기 목록 조회 성공", consultationService.getWaitingList(staffId, page)));
+    }
+
+    @GetMapping("/consultations")
+    public ResponseEntity<ApiResponse<ConsultationDetailResponse>> getDetail(
+            @AuthenticationPrincipal Long staffId,
+            @RequestParam Long id) {
+
+        return ResponseEntity.ok(
+                ApiResponse.ok("조회에 성공하였습니다.", consultationService.getDetail(staffId, id)));
     }
 }
